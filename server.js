860f40d5799ab741a852ae96ad322b05ad95cf25
truongvukt2000 <truongvukt2000@gmail.com>
2022-07-25 12:57:56 +0700

    fix server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import fileUpload from 'express-fileupload';
 import cookieParser from 'cookie-parser';
 import ConnectMongoDB from './src/database/mongodb';
 import mainRoutes from './src/routes';
-import path from 'path';
+// import path from 'path';
 
 dotenv.config({ path: './src/config/.env' });
 
@@ -41,12 +41,12 @@ app.post('/a', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
